fbad9cf8c10edf4d31f41ef10cb76cbc48554bd1
Melis Kara <meliskara-dev@.com>
2026-07-17 21:36:30 +0300

    impl. GlobalExceptionHandler for API error

---
+++ src/main/java/com/infina/concurrentcryptopricesimulator/api/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.infina.concurrentcryptopricesimulator.api;
+
+import com.infina.concurrentcryptopricesimulator.api.dto.ErrorResponseDto;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ErrorResponseDto> handleValidationError(ConstraintViolationException e){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDto(e.getMessage()));
+	}
+
+	@ExceptionHandler(SimulationAlreadyRunningException.class)
+	public ResponseEntity<ErrorResponseDto> handleSimulationAlreadyRunning(SimulationAlreadyRunningException e){
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponseDto(e.getMessage()));
+	}
+
+	@ExceptionHandler(NoSimulationYetException.class)
+	public ResponseEntity<ErrorResponseDto> handleNoSimulationYet(NoSimulationYetException e) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto(e.getMessage()));
+	}
+
+}
